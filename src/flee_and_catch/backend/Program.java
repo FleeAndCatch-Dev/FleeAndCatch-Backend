5f539fc119c0c6b6cb0a00482b66fdfb13b09acb
ThunderSL94 <SL-Thunder@gmx.de>
2016-12-01 13:33:11 +0100

    Home  - some bug fixes

--- src/flee_and_catch/backend/Program.java
+++ src/flee_and_catch/backend/Program.java
@@ -2,6 +2,10 @@ package flee_and_catch.backend;
 import java.io.IOException;
 
 import flee_and_catch.backend.communication.Server;
+import flee_and_catch.backend.communication.command.Identification;
+import flee_and_catch.backend.communication.command.Position;
+import flee_and_catch.backend.communication.command.Robot;
+import flee_and_catch.backend.robot.RobotController;
 
 public class Program {
 
@@ -17,6 +21,7 @@ public class Program {
 	public static void main(String[] args) {
 		try {
 			Server.open();
+			RobotController.getRobots().add(new Robot(new Identification(1, "192.168.0.1", 5000, "Robot", "ThreeWheelDrive"), new Position(0, 0, 0), 0));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
